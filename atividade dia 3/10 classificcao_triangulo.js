72c9903b8813dc970d04808ee46af6c422a6d425
migueljusto-2307 <justomiguel2307@gmail.com>
2026-08-05 13:57:39 -0300

    novas atividades

---
+++ atividade dia 3/10 classificcao_triangulo.js
@@ -0,0 +1,12 @@
+let lado1 = Number(prompt("Lado 1: "));
+let lado2 = Number(prompt("Lado 2: ")); 
+let lado3 = Number(prompt("Lado 3: ")); 
+let classe ;
+if (lado1 === lado2 && lado2 === lado3) {
+    classe = "Equilátero";
+} else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
+    classe = "Isósceles";
+} else {
+    classe = "Escaleno";
+}
+console.log(classe);
